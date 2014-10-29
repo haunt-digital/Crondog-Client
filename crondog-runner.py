a9c3575b1df65fa337bd91b24714404f36079753
thepearson <thepearson@gmail.com>
2014-10-29 16:47:08 +1300

    Initial commit

---
+++ crondog-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'craigp'
+
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+from crondogclient.crondog import main
+
+if __name__ == '__main__':
+    main()
